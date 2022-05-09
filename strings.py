77ca3a640ae86a67add64e22a2b5664e2258cb2b
stephenmyette <104920487+stephenmyette@users.noreply.github.com>
2022-05-09 11:57:13 -0400

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,29 @@
+"strings are a data type"
+"strings can be changed" 
+"strings are written within single or double quotation marks"
+"all users input are strings."
+    
+    
+
+string1= "this is a string."
+#string2 = "this is also a string."
+    
+# print(string1)
+
+
+#print (string2)     
+#var1 = input("please enter some text:")   
+#print("you entered: " + var1)
+
+print(string1[5])
+print(string1[0:15])
+print(string1[3:])
+print(string1[:])
+print(string1 * 2)
+
+
+var1 = ord('C')
+print(var1)
+
+var2 = chr(113)
+print(var2)
